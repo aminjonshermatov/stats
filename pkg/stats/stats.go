106bca3ebb9d9d5ebb4182a87ed5d773e6749a0e
aminjonshermatov <aminjonshermatov@gmail.com>
2021-04-02 22:56:32 +0500

    tests and funcs category

--- pkg/stats/stats.go
+++ pkg/stats/stats.go
@@ -35,3 +35,24 @@ func TotalInCategory(payments []types.Payment, category types.Category) types.Mo
 
 	return sum
 }
+
+func FilterByCategory(payments []types.Payment, category types.Category) []types.Payment {
+	var filtered []types.Payment
+	for _, payment := range payments {
+		if payment.Category == category {
+			filtered = append(filtered, payment)
+		}
+	}
+
+	return filtered
+}
+
+func CategoriesTotal(payments []types.Payment) map[types.Category]types.Money {
+	categories := map[types.Category]types.Money{}
+
+	for _, payment := range payments {
+		categories[payment.Category] += payment.Amount
+	}
+
+	return categories
+}
